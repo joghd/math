9715dce5b5ee7df807dc5b1494dd7947b94b326c
Iman Af <joghdorg@gmail.com>
2017-05-28 20:34:12 -0700

    removed blacked line

--- lnx.cpp
+++ lnx.cpp
@@ -1,12 +1,12 @@
-
 #include <iostream>
 using namespace std;
 double f(double x);
+
 int main(){
-  double sum = 0, deltax, x{50};
-  constexpr unsigned n{100000};
+  double sum{0}, deltax, x{50};
+  constexpr int n{100000};
   deltax = (x-1)/n;
-  for(auto i = 0; i < n; i++){
+  for(auto i{0}; i < n; i++){
     sum += deltax*(f(1+i*deltax));
   }
   cout << sum << endl;
